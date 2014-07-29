6deb255ee2be7b42c7d36aae7bdc5f2b7cbcb610
sarnobat <ss401533@gmail.com>
2014-07-28 17:46:19 -0700

    Initial commit

--- launch.groovy
+++ launch.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
@@ -8,7 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -30,7 +32,7 @@ import org.json.JSONObject;
 import com.sun.net.httpserver.HttpServer;
 
 public class ScriptLauncher {
-	private static String unixCommandString;
+	static String unixCommandString;
 
 	@Path("/")
 	public static class HelloWorldResource { // Must be public
@@ -59,51 +61,78 @@ public class ScriptLauncher {
 		public Response post(
 		// TODO: @QueryParam("rootId") Integer iRootId
 		) throws JSONException {
-			System.out.println("1");
-			System.err.println("1");
+			System.out.println("aaa");
+			try {
+				System.out.println("aa");
+				Process theProcess = Runtime.getRuntime().exec(ScriptLauncher.unixCommandString);//"echo 'goodbye'");
+				System.out.println("a");
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						theProcess.getInputStream()));
+				System.out.println("b");
+				try {
+					int returnCode = theProcess.waitFor();
+					System.out.println("c");
+					StringBuffer sb = new StringBuffer();
+					while(br.ready()) {
+						sb.append(br.readLine());						
+						System.out.println("d");
+					}
+					if (returnCode == 0) {
+						System.out.println("e");
+						JSONObject json = new JSONObject();
+						json.put("status", "success");
+						json.put("output", sb.toString());
+						return Response.ok().header("Access-Control-Allow-Origin", "*")
+								.entity(json.toString()).type("application/json").build();			
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+				e.printStackTrace(System.err);
+			}
+			//System.out.println("1");
+			//System.err.println("1");
 			JSONObject json = new JSONObject();
-			System.out.println("2");
+			//System.out.println("2");
 			json.put("method", "post");
-			System.out.println("3");
+			//System.out.println("3");
 			return Response.ok().header("Access-Control-Allow-Origin", "*")
 					.entity(json.toString()).type("application/json").build();
 		}
 	}
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception {
 
 		String portNumber = "";
-		// create the command line parser
-		Parser parser = new GnuParser();
+		options: {
+			Options options = new Options();
+			options.addOption(OptionBuilder.withLongOpt("port")
+					.withDescription("use PORT-byte blocks").hasArg()
+					.withArgName("PORT").create('p'));
+			getOptions: {
+				CommandLine line = new GnuParser().parse(options, args);
 
-		// create the Options
-		Options options = new Options();
-		options.addOption(
-				OptionBuilder.withLongOpt("port")
-				.withDescription("use PORT-byte blocks").hasArg()
-				.withArgName("PORT").create('p')
-				);
-//		options.addOption("p", "port", false,
-//				"do not hide entries starting with .");
-		// parse the command line arguments
-		CommandLine line = parser.parse(options, args);
+				System.out.println("leftovers: " + line.getArgs());
 
-		System.out.println("leftovers: " + line.getArgs());
+				if (line.hasOption("port")) {
+					portNumber = line.getOptionValue("port");
+					System.out.println(portNumber);
+				}
 
-
-		if (line.hasOption("port")) {
-			portNumber = line.getOptionValue("port");
-			System.out.println(portNumber);
-		}
-
-		if (line.getArgs().length > 1) {
-			portNumber = line.getArgs()[1];
-		}
-		if (line.getArgs().length > 0) {
-			ScriptLauncher.unixCommandString = line.getArgs()[0];
-		}
-		if (line.getArgs().length == 0) {
-			System.out.println("No command specified");
+				if (line.getArgs().length > 1) {
+					portNumber = line.getArgs()[1];
+				}
+				if (line.getArgs().length > 0) {
+					ScriptLauncher.unixCommandString = line.getArgs()[0];
+					System.out.println("command: " + ScriptLauncher.unixCommandString);
+				}
+				if (line.getArgs().length == 0) {
+					System.out.println("No command specified");
+				}
+			}
 		}
 		HttpServer server = JdkHttpServerFactory.createHttpServer(new URI(
 				"http://localhost:" + portNumber + "/"), new ResourceConfig(

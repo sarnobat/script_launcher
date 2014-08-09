97830eba233e30ea4bb5ac91bcec97e52bed23c6
sarnobat <ss401533@gmail.com>
2014-08-08 20:13:36 -0700

    Minor

--- run.sh
+++ run.sh
@@ -1,11 +1,23 @@
+
+
+while true
+do
+{   
+	# For openbsd, add -p to nc
+	 echo 'HTTP/1.0 200 OK
+Access-Control-Allow-Origin: *
+Content-Type: text/plain\n\n' \
+'{"Hello":"world"}' | cat - body.txt \
+	 | nc -l -p 4458
+}  
+done;
+
+
+
 # Works
 #my_netcat.sh 4458 | grep --line-buffered HTTP | grep --line-buffered POST
 my_netcat.sh 4458 | http_request_body.js | tee temp.log | xargs -n 1 cypher_query.sh
 
-{ 
-echo 'foo
-'; echo "bar" 
-} | nc -l -p 4459
  
 
 

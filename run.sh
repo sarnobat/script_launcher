f1adc486e80fb22e85a76c843d744e537c1c536b
sarnobat <ss401533@gmail.com>
2014-08-12 20:44:07 -0700

    Minor

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 while true; 
-do cat header20.txt fifo | nc -l -p 4458  | http_request_body.js | sed --unbuffered 's/i/o/' | xargs -n 1 cypher_query.sh > fifo; done;
+do cat header20.txt fifo | nc -l -p 4458  | http_request_body.js | sed --unbuffered 's/i/o/' | xargs -n 1 cypher_query.sh > fifo
+done;
 
 exit;
 

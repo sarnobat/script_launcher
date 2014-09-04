17cda41f384170fa8bea9f20e90c12556e953b8e
sarnobat <ss401533@gmail.com>
2014-09-03 20:38:15 -0700

    Minor

---
+++ client.sh
@@ -0,0 +1,2 @@
+ curl --header "Connection: close" --header "Content-Type:text/plain" --header "Content-Length:5" "http://netgear.rohidekar.com:4458" --data '45
+';
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ while true; do \
 	| tee cat_fifo_result.log \
  	| http_response_header_add.sh \
 	| nc -l -p 4458  \
+	| tee input.log \
 	| http_request_body.js \
 	| sed --unbuffered 's/i/o/' \
 	| xargs -n 1 get_urls.sh  \

c64d8df67f00e094f7183bfc2b3676b3d2730623
sarnobat <ss401533@gmail.com>
2014-09-16 22:08:58 -0700

    Minor

--- version1/get_urls.sh
+++ version1/get_urls.sh
@@ -4,10 +4,11 @@ read x ;
 #echo "root: $1 $x";
 #do 
 #exit;
-curl -d'{"query":"start n=node('$1') match n-[r:CONTAINS]->c where c.title? <> \"\" return \"title :\" + c.title?  limit 20;"}' \
+curl -d'{"query":"start n=node('$1') match n-[r:CONTAINS]->c where c.title? <> \"\" return c.title?  limit 2;"}' \
 	-H accept:text/plain \
 	-H content-type:application/json \
 	http://netgear.rohidekar.com:7474/db/data/cypher 2> /dev/null
+echo ""
 
 #done
 

6b054bfe60082824a40bc687e1ab008e99500004
sarnobat <ss401533@gmail.com>
2014-09-16 21:00:30 -0700

    Minor

---
+++ version1/http_request_body.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+var parser = require('http-string-parser');
+var readline = require('readline');
+var devnull = require('dev-null');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: devnull()
+});
+
+var bodyBegan = false;
+var contentLengthConsumed = 1;
+var contentLength;
+rl.on('line', function(line){
+
+	var resourceLine = line.match(/.*\/\?param1=(.*)HTTP\/1.1/);
+	if (resourceLine != null && resourceLine.length > 0) {
+		var paramValue = resourceLine[1];
+		//console.log("Param: " + paramValue);
+		//process.stdout.write(paramValue + '\n');
+	} else {
+		//console.log("Not a resource line: " + line +"\n");
+	}
+
+	console.warn('http_request_body.js [DEBUG] ' + line);
+	contentLengthLine = line.match(/Content-Length: (.*)/);
+	//console.warn("content length in header: " + contentLength);
+	//console.warn("content length consumed: " + contentLengthConsumed);
+	
+
+	if (contentLengthLine != null && contentLengthLine.length > 0) {
+		//console.warn(contentLengthLine);
+		contentLength = contentLengthLine[1];
+		//console.warn(contentLength);
+	}
+	if (bodyBegan) {
+		//	console.log('4');
+		process.stdout.write(line + '\n');
+		var contentLengthDelta = getByteLength(line);
+		contentLengthConsumed += contentLengthDelta;
+		//console.log(line);
+		console.warn('consumed ' + contentLengthConsumed);
+		if (contentLengthConsumed >= contentLength) {
+			//console.log('11');
+			console.warn('exiting');
+			bodyBegan = false;
+			contentLengthConsumed = 0;
+			contentLength = 0;
+			// If we exit, the process doesn't start again for followup requests
+			// if we use netcat's --keep-alive setting. We have to use a loop
+			process.exit(0);
+		}
+	}
+	//console.log('10');
+	bodyBegan = (bodyBegan || line == null || line == '');
+	
+}).on('close', function() {
+	console.warn('closing ');
+  //console.log('Have a great day!');
+  //bodyBegan = false;
+  //process.exit(0);
+});
+
+function getByteLength(normal_val) {
+    // Force string type
+    normal_val = String(normal_val);
+
+    var byteLen = 0;
+    for (var i = 0; i < normal_val.length; i++) {
+        var c = normal_val.charCodeAt(i);
+        byteLen += c < (1 <<  7) ? 1 :
+                   c < (1 << 11) ? 2 :
+                   c < (1 << 16) ? 3 :
+                   c < (1 << 21) ? 4 :
+                   c < (1 << 26) ? 5 :
+                   c < (1 << 31) ? 6 : Number.NaN;
+    }
+    return byteLen;
+}
---
+++ version1/http_response_header_add.sh
@@ -0,0 +1,27 @@
+echo "starting" > temp.log 
+
+body=""
+
+# problem: there is no end of input so we never out of this loop
+while read line; do  \
+	echo "Read a line\n" >> temp.log \
+	body="$body ${line}"; 
+done
+
+
+#body=`cat`
+
+echo "collected stdin" >> temp.log 
+
+body_size=`echo $body | http_content_size.js`
+
+echo "calculated body size " >> temp.log
+
+echo "HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+Content-length : $body_size
+Content-Type: application/json
+"
+echo $body
+
+echo "wrote response" >> temp.log

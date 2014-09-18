6665af959e259cea24fe6267702ec1e73e5997c3
Sridhar Sarnobat <ss401533@gmail.com>
2014-09-17 21:05:46 -0700

    Minor

--- version1/http_response_header_add.sh
+++ version1/http_response_header_add.sh
@@ -1,22 +1,22 @@
-echo "starting" >> temp.log 
+echo "starting" #>> temp.log 
 
 body=""
 
 # problem: there is no end of input so we never out of this loop
 while read line; do  \
-	echo "Read a line\n" >> temp.log 
+	echo "Read a line\n" #>> temp.log 
 	body="$body ${line}"; 
 done
 
 
 #body=`cat`
 
-echo "collected stdin" >> temp.log 
+echo "collected stdin" #>> temp.log 
 
 body_size_with_newlines=`echo $body | http_content_size.js | perl -pe 's{^\n}{}g'`
 body_size=$body_size_with_newlines
 
-echo "calculated body size " >> temp.log
+echo "calculated body size " #>> temp.log
 
 echo "HTTP/1.1 200 OK
 Access-Control-Allow-Origin: *
@@ -25,6 +25,6 @@ Content-Type: application/json
 "
 echo $body
 
-echo "Body is: " $body >> temp.log
+echo "Body is: " $body #>> temp.log
 
-echo "wrote response" >> temp.log
+echo "wrote response" #>> temp.log

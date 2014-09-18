7069f0464489603470d09467039933accd2a0ac6
Sridhar Sarnobat <ss401533@gmail.com>
2014-09-17 21:05:46 -0700

    Minor

--- version1/run.sh
+++ version1/run.sh
@@ -1,3 +1,6 @@
+cd /home/sarnobat/Desktop/new/github/script_launcher/version1 || exit "You need to cd to version1 first"
+echo "Now open http://netgear.rohidekar.com/launcher/"
+
 # tee -a must be used otherwise the file gets cleared before you can read it
 while true; do \
 	cat fifo \
@@ -7,3 +10,4 @@ while true; do \
 	| http_request_body.js \
 	| xargs -n 1 get_urls.sh  > fifo
 done;
+

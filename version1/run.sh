33a8e2f8c1ce6b6e3a950665c279d5be6e47b940
Sridhar Sarnobat <ss401533@gmail.com>
2014-09-17 21:05:46 -0700

    Minor

--- version1/run.sh
+++ version1/run.sh
@@ -1,12 +1,9 @@
+# tee -a must be used otherwise the file gets cleared before you can read it
 while true; do \
 	cat fifo \
-	| tee -a cat_fifo_result.log \
 	| neo_shell_to_yurl_json.js \
 	| http_response_header_add.sh \
-	| tee -a http_response.log \
 	| nc -l -p 4458  \
-	| tee -a input.log \
 	| http_request_body.js \
-	| sed --unbuffered 's/i/o/' \
 	| xargs -n 1 get_urls.sh  > fifo
 done;

ca3d818aa4de02182a99625e81ad655c2cfc9ae2
sarnobat <ss401533@gmail.com>
2014-11-21 22:08:15 -0800

    Minor

--- 2_echo_server/listen.sh
+++ 2_echo_server/listen.sh
@@ -3,10 +3,12 @@
 # - if you run a while loop in the background there is no easy way to kill it
 echo "http://netgear.rohidekar.com:4461?param1=Sridhar"
 #{
+#echo $PWD
 	while true; do \
+		echo $PWD
 		cat fifo \
 		| nc -l -p $1  \
-		| http_request_body.js \
+		| ./http_request_body.js \
 		| tee fifo
 	done;
 
